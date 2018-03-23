ee6a5efe9f1d5617d8380d941cf0b1aa4cfae367
lbeucherie <lbeucherie@ecocea.com>
2018-03-23 18:02:42 +0100

    Stabilization of command dir

--- shell.c
+++ shell.c
@@ -9,10 +9,10 @@ char* shellcommand(char* command )
 {
   printf("test du shell... : \n");
   FILE *fp;
-  char path[1035];          
+  char path[1035];
   char buffer[10000];       // Buffer
   char* finalOutput = NULL; // Final result
-  char* commandSplitted;   // Command splitted to check if cd  
+  char* commandSplitted;   // Command splitted to check if cd
   char* commandSplittedArray[2];
   char currentDirectory[100];
   memset(path,0,1035);      // reset to 0 path
@@ -20,7 +20,7 @@ char* shellcommand(char* command )
   memset(currentDirectory,0,100);  // reset to 0 currentDirectory
 
   int i =0;
- 
+
   /**
   Spliting command to see if there is change directory (cd)
   **/
@@ -28,28 +28,28 @@ char* shellcommand(char* command )
   while (commandSplitted != NULL)
   {
     commandSplittedArray[i] = commandSplitted;
-    commandSplitted = strtok (NULL, " "); 
+    commandSplitted = strtok (NULL, " ");
     i++;
   }
   printf("0 %s\n", commandSplittedArray[0]);
   printf("1 %s\n", commandSplittedArray[1]);
 
-  //If changing directory 
+  //If changing directory
   if (strcmp(commandSplittedArray[0],"cd") == 0){
     printf("%s\n", "on passe dans la command cd");
     if(chdir(commandSplittedArray[1]) == -1){
-       printf( "Unable to locate the directory: %s\n", commandSplittedArray[1]); 
+       printf( "Unable to locate the directory: %s\n", commandSplittedArray[1]);
     }
-  
+
      getcwd(currentDirectory,100);
-      printf("Current Working Directory : %s",currentDirectory);
+     printf("Current Working Directory : %s",currentDirectory);
      finalOutput = malloc((strlen(currentDirectory))*sizeof(char));
      if(finalOutput == NULL){
        printf("%s\n", "Failed to allocate memory");
-      //exit(0);
+      exit(0);
      }
-    // strcpy(finalOutput,buffer);
-   
+     strcpy(finalOutput,currentDirectory);
+
   }
    /* Open the command for reading. */
   else{
@@ -60,7 +60,7 @@ char* shellcommand(char* command )
       }
       else {
         /* Read the output a line at a time - output it. */
-        while (fgets(path, sizeof(path)-1, fp) != NULL) {      
+        while (fgets(path, sizeof(path)-1, fp) != NULL) {
           printf("%s", path);
           strcat(buffer,path);
         }
@@ -70,13 +70,13 @@ char* shellcommand(char* command )
          int size = strlen(buffer)*sizeof(char);
          printf("size of the buffer : %d\n", size);
 
-         finalOutput = malloc((strlen(buffer))*sizeof(char));
+         finalOutput = (char* )malloc((strlen(buffer))*sizeof(char) +1);
          if(finalOutput == NULL){
           printf("%s\n", "Failed to allocate memory");
-          //exit(0);
+          exit(0);
          }
-         //strcpy(finalOutput,buffer); //fais peter
-           
+         strcpy(finalOutput,buffer); //fais peter
+
      }
 
       /* close */
@@ -93,7 +93,6 @@ char* shellcommand(char* command )
 
   }
   printf("%s\n", "FREE MEMORY");
-  
   free(finalOutput);
-  
+
 }
